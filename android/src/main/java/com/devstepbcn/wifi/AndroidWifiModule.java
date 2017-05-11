e81ba17ff35fce6039302ba643a96eb51647b43f
Edward Paget <edward@cassetteta.pe>
2017-05-11 10:44:51 -0500

    Update android wifi to correctly bind/unbind after connecting

--- android/src/main/java/com/devstepbcn/wifi/AndroidWifiModule.java
+++ android/src/main/java/com/devstepbcn/wifi/AndroidWifiModule.java
@@ -12,15 +12,21 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import android.net.NetworkRequest;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 import android.net.wifi.WifiConfiguration;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.wifi.WifiInfo;
+import android.os.Build;
 import android.content.Context;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.text.TextUtils;
+
+import android.util.Log;
 import java.util.List;
 import java.lang.Thread;
 
@@ -30,207 +36,238 @@ import org.json.JSONObject;
 
 public class AndroidWifiModule extends ReactContextBaseJavaModule {
 
-	//WifiManager Instance
-	WifiManager wifi;
+    public static final String LOG_TAG = "MyFlair";
 
-	//Constructor
-	public AndroidWifiModule(ReactApplicationContext reactContext) {
-		super(reactContext);
-		wifi = (WifiManager)reactContext.getSystemService(Context.WIFI_SERVICE);
-	}
+    //WifiManager Instance
+    WifiManager wifi;
+    WifiLock lock;
+    ConnectivityManager connectivityManager;
 
-	//Name for module register to use:
-	@Override
-	public String getName() {
-		return "AndroidWifiModule";
-	}
+    //Constructor
+    public AndroidWifiModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        wifi = (WifiManager)reactContext.getSystemService(Context.WIFI_SERVICE);
+        connectivityManager = (ConnectivityManager) reactContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
 
-	//Method to load wifi list into string via Callback. Returns a stringified JSONArray
-	@ReactMethod
-	public void loadWifiList(Callback successCallback, Callback errorCallback) {
-		try {
-			List < ScanResult > results = wifi.getScanResults();
-			JSONArray wifiArray = new JSONArray();
+    //Name for module register to use:
+    @Override
+    public String getName() {
+        return "AndroidWifiModule";
+    }
 
-			for (ScanResult result: results) {
-				JSONObject wifiObject = new JSONObject();
-				if(!result.SSID.equals("")){
-					try {
-                                            wifiObject.put("SSID", result.SSID);
-                                            wifiObject.put("BSSID", result.BSSID);
-                                            wifiObject.put("capabilities", result.capabilities);
-                                            wifiObject.put("frequency", result.frequency);
-                                            wifiObject.put("level", result.level);
-                                            wifiObject.put("timestamp", result.timestamp);
-                                            //Other fields not added
-                                            //wifiObject.put("operatorFriendlyName", result.operatorFriendlyName);
-                                            //wifiObject.put("venueName", result.venueName);
-                                            //wifiObject.put("centerFreq0", result.centerFreq0);
-                                            //wifiObject.put("centerFreq1", result.centerFreq1);
-                                            //wifiObject.put("channelWidth", result.channelWidth);
-					} catch (JSONException e) {
-                                            errorCallback.invoke(e.getMessage());
-					}
-					wifiArray.put(wifiObject);
-				}
-			}
-			successCallback.invoke(wifiArray.toString());
-		} catch (IllegalViewOperationException e) {
-			errorCallback.invoke(e.getMessage());
-		}
-	}
+    //Method to load wifi list into string via Callback. Returns a stringified JSONArray
+    @ReactMethod
+    public void loadWifiList(Callback successCallback, Callback errorCallback) {
+        try {
+            List < ScanResult > results = wifi.getScanResults();
+            JSONArray wifiArray = new JSONArray();
 
-	//Method to check if wifi is enabled
-	@ReactMethod
-	public void isEnabled(Callback isEnabled) {
-		isEnabled.invoke(wifi.isWifiEnabled());
-	}
-
-	//Method to connect/disconnect wifi service
-	@ReactMethod
-	public void setEnabled(Boolean enabled) {
-		wifi.setWifiEnabled(enabled);
-	}
-
-	//Send the ssid and password of a Wifi network into this to connect to the network.
-	//Example:  wifi.findAndConnect(ssid, password);
-	//After 10 seconds, a post telling you whether you are connected will pop up.
-	//Callback returns true if ssid is in the range
-	@ReactMethod
-	public void findAndConnect(String ssid, String password, Callback ssidFound) {
-		List < ScanResult > results = wifi.getScanResults();
-		boolean connected = false;
-		for (ScanResult result: results) {
-			String resultString = "" + result.SSID;
-			if (ssid.equals(resultString)) {
-				connected = connectTo(result, password, ssid);
-			}
-		}
-		ssidFound.invoke(connected);
-	}
-
-	//Use this method to check if the device is currently connected to Wifi.
-	@ReactMethod
-	public void connectionStatus(Callback connectionStatusResult) {
-		ConnectivityManager connManager = (ConnectivityManager) getReactApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		if (mWifi.isConnected()) {
-			connectionStatusResult.invoke(true);
-		} else {
-			connectionStatusResult.invoke(false);
-		}
-	}
-
-	//Method to connect to WIFI Network
-	public Boolean connectTo(ScanResult result, String password, String ssid) {
-		//Make new configuration
-		WifiConfiguration conf = new WifiConfiguration();
-		conf.SSID = "\"" + ssid + "\"";
-		String Capabilities = result.capabilities;
-		if (Capabilities.contains("WPA2")) {
-			conf.preSharedKey = "\"" + password + "\"";
-		} else if (Capabilities.contains("WPA")) {
-			conf.preSharedKey = "\"" + password + "\"";
-		} else if (Capabilities.contains("WEP")) {
-			conf.wepKeys[0] = "\"" + password + "\"";
-			conf.wepTxKeyIndex = 0;
-			conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-			conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-		} else {
-			conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-		}
-		//Remove the existing configuration for this netwrok
-		List<WifiConfiguration> mWifiConfigList = wifi.getConfiguredNetworks();
-		String comparableSSID = ('"' + ssid + '"'); //Add quotes because wifiConfig.SSID has them
-		int updateNetwork = -1;
-		for(WifiConfiguration wifiConfig : mWifiConfigList){
-			if(wifiConfig.SSID.equals(comparableSSID)){
-				conf.networkId = wifiConfig.networkId;
-				updateNetwork = wifi.updateNetwork(conf);
-			}
-		}
-
-                // If network not already in configured networks add new network
-		if ( updateNetwork == -1 ) {
-                        updateNetwork = wifi.addNetwork(conf);
-		};
-
-                if ( updateNetwork == -1 ) {
-                  return false;
+            for (ScanResult result: results) {
+                JSONObject wifiObject = new JSONObject();
+                if(!result.SSID.equals("")){
+                    try {
+                        wifiObject.put("SSID", result.SSID);
+                        wifiObject.put("BSSID", result.BSSID);
+                        wifiObject.put("capabilities", result.capabilities);
+                        wifiObject.put("frequency", result.frequency);
+                        wifiObject.put("level", result.level);
+                        wifiObject.put("timestamp", result.timestamp);
+                        //Other fields not added
+                        //wifiObject.put("operatorFriendlyName", result.operatorFriendlyName);
+                        //wifiObject.put("venueName", result.venueName);
+                        //wifiObject.put("centerFreq0", result.centerFreq0);
+                        //wifiObject.put("centerFreq1", result.centerFreq1);
+                        //wifiObject.put("channelWidth", result.channelWidth);
+                    } catch (JSONException e) {
+                        errorCallback.invoke(e.getMessage());
+                    }
+                    wifiArray.put(wifiObject);
                 }
+            }
+            successCallback.invoke(wifiArray.toString());
+        } catch (IllegalViewOperationException e) {
+            errorCallback.invoke(e.getMessage());
+        }
+    }
 
-                boolean disconnect = wifi.disconnect();
-		if ( !disconnect ) {
-			return false;
-		};
+    //Method to check if wifi is enabled
+    @ReactMethod
+    public void isEnabled(Callback isEnabled) {
+        isEnabled.invoke(wifi.isWifiEnabled());
+    }
 
-		boolean enableNetwork = wifi.enableNetwork(updateNetwork, true);
-		if ( !enableNetwork ) {
-			return false;
-		};
+    //Method to connect/disconnect wifi service
+    @ReactMethod
+    public void setEnabled(Boolean enabled) {
+        wifi.setWifiEnabled(enabled);
+    }
 
-		return true;
-	}
+    //Send the ssid and password of a Wifi network into this to connect to the network.
+    //Example:  wifi.findAndConnect(ssid, password);
+    //After 10 seconds, a post telling you whether you are connected will pop up.
+    //Callback returns true if ssid is in the range
+    @ReactMethod
+    public void findAndConnect(String ssid, String password, Callback ssidFound) {
+        List < ScanResult > results = wifi.getScanResults();
+        boolean connected = false;
+        for (ScanResult result: results) {
+            String resultString = "" + result.SSID;
+            if (ssid.equals(resultString)) {
+                Log.d(LOG_TAG, "Found WiFi");
+                connectTo(result, password, '"' + ssid + '"', ssidFound);
+                break;
+            }
+        }
+    }
 
-	//Disconnect current Wifi.
-	@ReactMethod
-	public void disconnect() {
-		wifi.disconnect();
-	}
+    //Use this method to check if the device is currently connected to Wifi.
+    @ReactMethod
+    public void connectionStatus(Callback connectionStatusResult) {
+        ConnectivityManager connManager = (ConnectivityManager) getReactApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mWifi.isConnected()) {
+            connectionStatusResult.invoke(true);
+        } else {
+            connectionStatusResult.invoke(false);
+        }
+    }
 
-	//This method will return current ssid
-	@ReactMethod
-	public void getSSID(final Callback callback) {
-		WifiInfo info = wifi.getConnectionInfo();
+    //Method to connect to WIFI Network
+    public void connectTo(ScanResult result, String password, final String ssid, final Callback ssidFound) {
+        //Make new configuration
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = ssid;
+        String Capabilities = result.capabilities;
+        if (Capabilities.contains("WPA2")) {
+            conf.preSharedKey = "\"" + password + "\"";
+        } else if (Capabilities.contains("WPA")) {
+            conf.preSharedKey = "\"" + password + "\"";
+        } else if (Capabilities.contains("WEP")) {
+            conf.wepKeys[0] = "\"" + password + "\"";
+            conf.wepTxKeyIndex = 0;
+            conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+        } else {
+            conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        }
+        int networkId = wifi.addNetwork(conf);
+        //Remove the existing configuration for this netwrok
+        if (networkId != -1) {
+            connect(networkId, ssid, ssidFound);
+        } else {
+            List<WifiConfiguration> mWifiConfigList = wifi.getConfiguredNetworks();
+            for( WifiConfiguration i : mWifiConfigList) {
+                if(i.SSID != null && i.SSID.equals(ssid)) {
+                    connect(i.networkId, ssid, ssidFound);
+                    break;
+                }
+            }
+        }
+    }
 
-		// This value should be wrapped in double quotes, so we need to unwrap it.
-		String ssid = info.getSSID();
-		if (ssid.startsWith("\"") && ssid.endsWith("\"")) {
-			ssid = ssid.substring(1, ssid.length() - 1);
-		}
+    public void connect(int networkId, final String ssid, final Callback ssidFound) {
+        lock = wifi.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "MyFlair Lock");
+        lock.acquire();
 
-		callback.invoke(ssid);
-	}
+        boolean enableNetwork = wifi.enableNetwork(i.networkId, true);
+        if ( !enableNetwork ) {
+            Log.d(LOG_TAG, "Failed to enable");
+            ssidFound.invoke(false);
+        };
 
-	//This method will return the basic service set identifier (BSSID) of the current access point
-	@ReactMethod
-	public void getBSSID(final Callback callback) {
-		WifiInfo info = wifi.getConnectionInfo();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            NetworkRequest.Builder builder = new NetworkRequest.Builder();
+            builder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
 
-		String bssid = info.getBSSID();
+            connectivityManager.requestNetwork(builder.build(), new ConnectivityManager.NetworkCallback() {
+                    @Override
+                    public void onAvailable(Network network) {
+                        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(network);
+                        if (TextUtils.equals(networkInfo.getExtraInfo(), ssid)) {
+                            connectivityManager.unregisterNetworkCallback(this);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                Log.d(LOG_TAG, "Bound 2: " + networkInfo.getExtraInfo());
+                                connectivityManager.bindProcessToNetwork(network);
+                            } else {
+                                ConnectivityManager.setProcessDefaultNetwork(network);
+                            }
+                            try {
+                                ssidFound.invoke(true);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+        }
+    }
 
-		callback.invoke(bssid.toUpperCase());
-	}
+    //Disconnect current Wifi.
+    @ReactMethod
+    public void disconnect() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            connectivityManager.bindProcessToNetwork(null);
+        } else {
+            ConnectivityManager.setProcessDefaultNetwork(null);
+        }
+        wifi.disconnect();
+        lock.release();
+    }
 
-	//This method will return current wifi signal strength
-	@ReactMethod
-	public void getCurrentSignalStrength(final Callback callback) {
-		int linkSpeed = wifi.getConnectionInfo().getRssi();
-		callback.invoke(linkSpeed);
-	}
-	//This method will return current IP
-	@ReactMethod
-	public void getIP(final Callback callback) {
-		WifiInfo info = wifi.getConnectionInfo();
-		String stringip=longToIP(info.getIpAddress());
-		callback.invoke(stringip);
-	}
+    //This method will return current ssid
+    @ReactMethod
+    public void getSSID(final Callback callback) {
+        WifiInfo info = wifi.getConnectionInfo();
 
-	public static String longToIP(int longIp){
-		StringBuffer sb = new StringBuffer("");
-		String[] strip=new String[4];
-		strip[3]=String.valueOf((longIp >>> 24));
-		strip[2]=String.valueOf((longIp & 0x00FFFFFF) >>> 16);
-		strip[1]=String.valueOf((longIp & 0x0000FFFF) >>> 8);
-		strip[0]=String.valueOf((longIp & 0x000000FF));
-		sb.append(strip[0]);
-		sb.append(".");
-		sb.append(strip[1]);
-		sb.append(".");
-		sb.append(strip[2]);
-		sb.append(".");
-		sb.append(strip[3]);
-		return sb.toString();
-	}
+        // This value should be wrapped in double quotes, so we need to unwrap it.
+        String ssid = info.getSSID();
+        if (ssid.startsWith("\"") && ssid.endsWith("\"")) {
+            ssid = ssid.substring(1, ssid.length() - 1);
+        }
+
+        callback.invoke(ssid);
+    }
+
+    //This method will return the basic service set identifier (BSSID) of the current access point
+    @ReactMethod
+    public void getBSSID(final Callback callback) {
+        WifiInfo info = wifi.getConnectionInfo();
+
+        String bssid = info.getBSSID();
+
+        callback.invoke(bssid.toUpperCase());
+    }
+
+    //This method will return current wifi signal strength
+    @ReactMethod
+    public void getCurrentSignalStrength(final Callback callback) {
+        int linkSpeed = wifi.getConnectionInfo().getRssi();
+        callback.invoke(linkSpeed);
+    }
+    //This method will return current IP
+    @ReactMethod
+    public void getIP(final Callback callback) {
+        WifiInfo info = wifi.getConnectionInfo();
+        String stringip=longToIP(info.getIpAddress());
+        callback.invoke(stringip);
+    }
+
+    public static String longToIP(int longIp){
+        StringBuffer sb = new StringBuffer("");
+        String[] strip=new String[4];
+        strip[3]=String.valueOf((longIp >>> 24));
+        strip[2]=String.valueOf((longIp & 0x00FFFFFF) >>> 16);
+        strip[1]=String.valueOf((longIp & 0x0000FFFF) >>> 8);
+        strip[0]=String.valueOf((longIp & 0x000000FF));
+        sb.append(strip[0]);
+        sb.append(".");
+        sb.append(strip[1]);
+        sb.append(".");
+        sb.append(strip[2]);
+        sb.append(".");
+        sb.append(strip[3]);
+        return sb.toString();
+    }
 }
 
